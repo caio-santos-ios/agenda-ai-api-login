fb4490e28bde74c135206fbf2be373fb4cf4e0c0
caiosantos.dev@outlook.com <seu@email.com>
2024-02-06 17:35:23 -0300

    feat: inserindo build.sh

--- src/account.controller.ts
+++ src/account.controller.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from "express";
 import accountServices from "./account.services";
 
-const create = (req: Request, res: Response): Response => {
-    const account = accountServices.create(req.body)
+const create = async (req: Request, res: Response): Promise<Response> => {
+    const account = await accountServices.create(req.body)
 
     return res.status(201).json({msg: "Rota post funcionando"})
 }
 
-const read = (req: Request, res: Response): Response => {
-    const accounts = accountServices.read()
+const read = async (req: Request, res: Response): Promise<Response> => {
+    const accounts = await accountServices.read()
 
-    return res.status(200).json({msg: "Rota get funcionando"})
+    return res.status(200).json(accounts)
 }
 
 export default { create, read }
--- src/account.services.ts
+++ src/account.services.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express"
+import { prisma } from "./database/prisma"
 
 const create = (dataAccount: any): any => {
 
     return dataAccount
 }
 
-const read = () => {
-    return
+const read = async () => {
+    const accounts = await prisma.account.findMany()
+
+    return accounts
 }
 
 export default { create, read }
