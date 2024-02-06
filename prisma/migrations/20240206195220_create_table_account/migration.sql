1e725f095b7eb982bbe63efeb14e341c5cb5420a
caiosantos.dev@outlook.com <seu@email.com>
2024-02-06 16:59:17 -0300

    feat: colocando db

---
+++ prisma/migrations/20240206195220_create_table_account/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isCollaborator" BOOLEAN NOT NULL DEFAULT false,
+    "accountValidated" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_email_key" ON "accounts"("email");
