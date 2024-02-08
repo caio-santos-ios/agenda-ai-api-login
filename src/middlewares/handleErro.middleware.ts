ba60f6cd5cdacc80f58f6b4478c5bb1d21dbc1de
caiosantos.dev@outlook.com <seu@email.com>
2024-02-08 13:02:44 -0300

    feat: incluindo handleErro

--- src/middlewares/handleErro.middleware.ts
+++ src/middlewares/handleErro.middleware.ts
@@ -4,36 +4,19 @@ import { ZodError } from 'zod'
 import { JsonWebTokenError } from 'jsonwebtoken'
 import { AppErro } from '../utils/AppErro'
 
-const handleErrorMiddleware = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Response => {
+const handleErrorMiddleware = ( err: Error, req: Request, res: Response, next: NextFunction ): Response => {
   if (err instanceof AppErro) {
     return res.status(err.status).json({ message: err.message })
-  }
-
-  if (err instanceof EntityNotFoundError) {
-    return res.status(404).json({ message: 'Not found' })
   }
 
   if (err instanceof ZodError) {
     return res.status(400).json({ message: err.flatten().fieldErrors });
   }
 
-  if (err instanceof QueryFailedError && err.message.includes('unique')) {
-    const message: string = err.driverError.detail
-    return res.status(409).json({ message })
-  }
-
   if (err instanceof JsonWebTokenError) {
     return res.status(409).json({ message: err.message })
   }
 
-  if (err instanceof TypeError) {
-    return res.status(409).json({ message: err.message })
-  }
   if (err instanceof Error) {
     return res.status(409).json({ message: err.message })
   }
